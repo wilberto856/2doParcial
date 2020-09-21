f9f2d984260b4488ee0fef3931accf4955cd35ab
Wilson Luna <42857108+wilberto856@users.noreply.github.com>
2020-09-20 18:29:45 -0600

    Add files via upload

---
+++ Spring-Cloud-router/src/main/java/com/eureka/server/SpringCloudRouterApplication.java
@@ -0,0 +1,13 @@
+package com.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudRouterApplication.class, args);
+	}
+
+}
